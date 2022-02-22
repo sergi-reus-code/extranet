2e68b2ad6f1a7b328882da576114cba9ec21fc31
Sergi Reus <sergireus28@gmail.com>
2022-02-22 10:04:18 +0100

    dasd

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -22,11 +22,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 
 /*
-app.use('/', (req,res)=> {
+app.get('/', (req,res)=> {
   console.log(req);
-})
-*/
-app.use('/users', usersRouter);
+  res.send("dsafasdfasdf")
+})*/
+
+//app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
