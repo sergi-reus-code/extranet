b952c46dc41eac1d717c7a58ca1982d13dc4be18
sergi-reus-code <sergireus28@gmail.com>
2022-02-22 11:07:51 +0100

    h

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/',function (req,res,next) {
-    res.send("Bienvenido a Reus Contruccions ");
+    res.render('users/index');
 
 
 
