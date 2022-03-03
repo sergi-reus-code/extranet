e4eecb1b0d5389e9452104e06eb8efc884d7eab5
Sergi Reus <sergireus28@gmail.com>
2022-03-03 10:18:37 +0100

    asdfd

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,20 @@ var router = express.Router();
 
 
 /* GET home page. */
+/*
 router.get('/',function (req,res,next) {
 
     var titulo = "TITULO DE index por que me da la gana"
     res.render('./index', { title: titulo });
 
 });
+*/
+router.get('/',function (req,res,next) {
 
+    var titulo = "TITULO DE index por que me da la gana"
+    res.send('../public/signin.html');
 
+});
 
 
 module.exports = router;
