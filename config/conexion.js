d712bd11d84a58c400ea80aa0034c32cd16c21f3
Sergi Reus <sergireus28@gmail.com>
2022-03-14 10:32:35 +0100

    rygf+

--- config/conexion.js
+++ config/conexion.js
@@ -7,6 +7,7 @@ var con= mysql.createConnection({
 }
 );
 
+/*
 con.connect( 
      (err)=>{
         if (!err){
@@ -20,7 +21,7 @@ con.connect(
 ); 
 
 
-
+*/
 
 
 
@@ -39,6 +40,18 @@ conDO.connect(
 (err)=>{
   if (!err){
      console.log('Conexión establecida to Digital Ocean')
+
+
+     
+     
+      conDO.query("CREATE TABLE `usuarios` (`id_usuario` int(11) NOT NULL,`nombre` varchar(100) NOT NULL,`apellidos` varchar(100) NOT NULL,`fecha_creacion` date NOT NULL,`username` varchar(100) NOT NULL,`password` varchar(100) NOT NULL,`telf` varchar(100) NOT NULL,`email` varchar(100) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", (data)=>{
+
+         console.log(data);
+
+      })
+
+
+
   }else{
      console.log('Error de conexión to Digital Ocean')
      console.log(err);
