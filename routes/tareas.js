936f8ac465b145da2fdf8c241242d4a6aa4cbf88
sergi-reus-code <sergi@reusconstruccions.com>
2022-03-24 19:10:17 +0100

    cnbmbm

--- routes/tareas.js
+++ routes/tareas.js
@@ -13,7 +13,35 @@ var router = express.Router();
 
                 console.log("estoy Tareas");
                 var titulo = "TITULO DE index por que me da la gana"
-                res.render('./task/tareas', { title: titulo });
+
+                 
+  
+                //Datos en array de objetos JSON
+                var datos = [{
+                    
+                    id_tarea : "1asdf",
+                    TituloTarea : "asdf",
+                    Descripción : "asdfas",
+                    Fecha : "asdfasd",
+                    IdTarea : "dasf"            
+                },{
+                    
+                id_tarea : "2asdf",
+                TituloTarea : "2asdf",
+                Descripción : "2asdfas",
+                Fecha : "2asdfasd",
+                IdTarea : "d2asf"            
+                }
+        
+        
+        
+        
+        
+        
+        ]   
+
+
+                res.render('./task/tareas', { tareas: datos });
             
             
             
