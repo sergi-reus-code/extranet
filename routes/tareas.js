315da455d732f860ad217dcc82556fbc4add589b
sergi-reus-code <sergireus28@gmail.com>
2022-03-24 09:56:43 +0100

    fhdhfd

--- routes/tareas.js
+++ routes/tareas.js
@@ -3,24 +3,24 @@ const req = require('express/lib/request');
 var router = express.Router();
 
 
+        router.get('/',(req,res) => {
 
-router.get('/',(req,res,next) => {
-
-        //console.log(req.headers);
-
-        res.send(req.headers)
-
-
-
-})
+                console.log("estoy en login");
+                var titulo = "TITULO DE index por que me da la gana"
+                res.render('./task/tareas', { title: titulo });
+            
+            
+            
+            });
 
 
-router.get('/pepepe',(req,res) => {
 
+
+    
+    
         
 
 
-})
 
 
 
