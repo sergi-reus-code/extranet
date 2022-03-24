2131ad0ad18dcf2ae0fcb949f1a2c7b6350f5cd5
sergi-reus-code <sergireus28@gmail.com>
2022-03-24 11:22:23 +0100

    df

--- app.js
+++ app.js
@@ -114,3 +114,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+we
--- routes/tareas.js
+++ routes/tareas.js
@@ -1,11 +1,11 @@
 var express = require('express');
-const req = require('express/lib/request');
 var router = express.Router();
 
+const logUtils= require("../DB_Controllers/loginController");
 
         router.get('/',(req,res) => {
 
-                console.log("estoy en login");
+                console.log("estoy Tareas");
                 var titulo = "TITULO DE index por que me da la gana"
                 res.render('./task/tareas', { title: titulo });
             
