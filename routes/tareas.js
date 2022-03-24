930243a76e2afd5e679109d78f01b04aea8e8b2d
Sergi Reus <sergireus28@gmail.com>
2022-03-24 12:14:50 +0100

    asdf

--- routes/tareas.js
+++ routes/tareas.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-const logUtils= require("../DB_Controllers/loginController");
+//const logUtils= require("../DB_Controllers/loginController");
 
         router.get('/',(req,res) => {
+
+               // Agafar les tareas de SQL i ficar-les amb JSON
+               // Pasar jason a EJS - tareas         
+
+
+
 
                 console.log("estoy Tareas");
                 var titulo = "TITULO DE index por que me da la gana"
