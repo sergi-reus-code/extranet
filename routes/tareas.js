d42fc893f7a526f59d02d770886182dc84f5436a
Sergi Reus <sergireus28@gmail.com>
2022-03-25 08:55:20 +0100

    <zc

--- routes/tareas.js
+++ routes/tareas.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-const logUtils= require("../DB_Controllers/loginController");
+//const logUtils= require("../DB_Controllers/loginController");
 
         router.get('/',(req,res) => {
 
+
+
+                var tareas = {
+                        id_tarea : "uid",
+                        TituloTarea : "sdfsdf",
+                        Descripción : "sdsdf",
+                        Fecha : "dsdfs",
+                        IdTarea : "sdfasdf"    }
+
                 console.log("estoy Tareas");
-                var titulo = "TITULO DE index por que me da la gana"
-                res.render('./task/tarea', { title: titulo });
+                //var datos = "RESULTADO CONSULTA SQL"
+                res.render('./task/tarea', { tareas: tareas });
             
                       });
 
