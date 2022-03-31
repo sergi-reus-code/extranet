383edb07977282c3b262051064823cbc9b7a4e9b
sergi-reus-code <sergireus28@gmail.com>
2022-03-31 10:28:13 +0200

    grghr

--- routes/error.js
+++ routes/error.js
@@ -17,7 +17,7 @@ router.get('/badpassword',function (req,res,next) {
     console.log("estoy en error de bad password");
 
     var titulo = "TITULO DE index por que me da la gana"
-    //res.render('./err/error', { title: titulo });
+    res.render('./err/error1', { title: titulo });
 
 });
 
