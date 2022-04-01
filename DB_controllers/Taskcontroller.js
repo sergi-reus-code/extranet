42454998cb0bd5ee55672a76d7e1afbeac8fcb21
Sergi Reus <sergireus28@gmail.com>
2022-04-01 12:16:30 +0200

    gfhj

--- DB_controllers/Taskcontroller.js
+++ DB_controllers/Taskcontroller.js
@@ -4,7 +4,6 @@ var conexion = require('../config/conexion');
 
 async function listTask(HTMLid_tarea){
   
-<<<<<<< HEAD
     //const SQL_query = "SELECT * FROM `tareas` WHERE `tareas` = 1";  
 
     const SQL_query = "SELECT `tareas`.* FROM `tareas`";
@@ -14,22 +13,6 @@ async function listTask(HTMLid_tarea){
     //console.log(result)  
    
   return result
-=======
-    const SQL_query = "SELECT * FROM `tareas` WHERE `id_tarea` = '"  + `${HTMLid_tarea}` + "'";   
-
-    const result = await conexion.query(SQL_query).catch(err => { throw err}); 
-
-    console.log(result)  
-  
-    
-
-
-    }
-
-    
-
-
->>>>>>> c330eaa76409138a37ee7dc66ebc201476aa174b
 
 
 
