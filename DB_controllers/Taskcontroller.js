c330eaa76409138a37ee7dc66ebc201476aa174b
sergi-reus-code <sergireus28@gmail.com>
2022-04-01 11:53:18 +0200

    fv fvfed

--- DB_controllers/Taskcontroller.js
+++ DB_controllers/Taskcontroller.js
@@ -2,17 +2,23 @@ const res = require('express/lib/response');
 var conexion = require('../config/conexion');
 
 
-async function listTask(HTMLusername){
+async function listTask(HTMLid_tarea){
   
-    const SQL_query = "SELECT * FROM `tareas` WHERE `tareas` = '"  + `${HTMLtareas}` + "'";   
+    const SQL_query = "SELECT * FROM `tareas` WHERE `id_tarea` = '"  + `${HTMLid_tarea}` + "'";   
 
     const result = await conexion.query(SQL_query).catch(err => { throw err}); 
 
     console.log(result)  
-   
-  return "tareas"
+  
+    
 
-}
+
+    }
+
+    
+
+
+
 
 
 module.exports = {
