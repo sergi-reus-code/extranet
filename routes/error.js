1e126304fb4983ade8357d4de18324f6f17c5e9f
sergi-reus-code <sergireus28@gmail.com>
2022-04-19 12:26:07 +0200

    hola

---
+++ routes/error.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* HOME PAGE */
+
+router.get('/badusername/',function (req,res,next) {
+
+    console.log("estoy en error de bad username");
+
+    var titulo = "TITULO DE index por que me da la gana"
+    res.render('./err/error', { title: titulo });
+
+});
+router.get('/badpassword',function (req,res,next) {
+
+    console.log("estoy en error de bad password");
+
+    var titulo = "TITULO DE index por que me da la gana"
+    res.render('./err/error1', { title: titulo });
+
+});
+
+module.exports = router;
