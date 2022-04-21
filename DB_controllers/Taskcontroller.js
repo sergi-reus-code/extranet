7a40ff2e1482e8b4609e78b92cc3b7a574b8e342
sergi-reus-code <sergi@reusconstruccions.com>
2022-04-21 08:09:06 +0200

    asdf

--- DB_controllers/Taskcontroller.js
+++ DB_controllers/Taskcontroller.js
@@ -4,8 +4,10 @@ var conexion = require('../config/conexion');
 
 async function listTask(HTMLid_tarea){
   
-    const SQL_query = "SELECT * FROM `tareas` WHERE `tareas`.`id_tarea` = 2";  
+    HTMLid_tarea = 2;
 
+    const SQL_query = "SELECT * FROM `tareas` WHERE `tareas`.`id_tarea` = " + HTMLid_tarea ;  
+    //const SQL_query = "SELECT * FROM `tareas` WHERE `tareas`.`id_tarea` = 2" ;
     //const SQL_query = "SELECT `tareas`.* FROM `tareas`";
 
     const result = await conexion.query(SQL_query).catch(err => { throw err}); 
