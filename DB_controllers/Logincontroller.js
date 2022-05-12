4cffd9a725130a6c7c0222bf8dd750393af525e9
sergi-reus-code <sergi@reusconstruccions.com>
2022-05-12 07:54:05 +0200

    cvvxccb

--- DB_controllers/Logincontroller.js
+++ DB_controllers/Logincontroller.js
@@ -3,42 +3,7 @@ var conexion = require('../config/conexion');
 
 
 
-async function readUserData(HTMLusername){
-  
-    const SQL_query = "SELECT * FROM `usuarios` WHERE `username` = '"  + `${HTMLusername}` + "'";   
-    const result = await conexion.query(SQL_query).catch(err => { throw err}); 
 
-    
-
-
-    if (result.length == 1) {
-   
-      var DBpassword = result[0].password
-
-      if (DBpassword == HTMLpassword) {
-
-        //console.log("son iguales!!!!");
-        
-        return "ok"
-        
-      } else {
-
-          //console.log("password no coinciden");
-          
-          return "badPassword"
-
-      }
-
-    } else {
-
-      //console.log("usuario no coincide");
-     
-      return "badUsername"
-
-
-    }
-
-}
 
 
 
@@ -82,6 +47,6 @@ async function checkUser(HTMLusername, HTMLpassword){
 
 module.exports = {
 
-  checkUser, readUserData
+  checkUser
 
 }
