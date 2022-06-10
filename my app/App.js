f5a66f4d583863b1949f5b7305d2d54ec28253b1
sergi-reus-code <sergireus28@gmail.com>
2022-06-10 15:34:11 +0200

    fghjgkyretw

---
+++ my app/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {NavigationContainer} from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import HomeScreen from './screens/HomeScreen'
+import TaskFormScreen from './screens/TaskFormScreen'
+
+const Stack = createStackNavigator()
+const App = () => { 
+    return(
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="HomeScreen" component={HomeScreen}/>
+            <Stack.Screen name="TaskFormScrenn" component={TaskFormScreen}/>   
+          </Stack.Navigator>
+        </NavigationContainer>
+      )
+}
+
+export default App
