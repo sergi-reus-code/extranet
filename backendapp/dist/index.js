cf156b5ad2747e6ad136adbd81c6ecfa635fe563
sergi-reus-code <sergi@reusconstruccions.com>
2022-06-12 18:56:12 +0200

    -,m.m,.-m,.

--- backendapp/dist/index.js
+++ backendapp/dist/index.js
@@ -1,4 +1,9 @@
 "use strict";
 
-var greet = 'hello worl';
-Console.log(greet);
+var _app = _interopRequireDefault(require("./app.js"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+_app["default"].listen(3001);
+
+console.log('server on port 3001');
