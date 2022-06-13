b8cd2ba59b89e5064e500e0afbe7ce91cc467cd1
sergi-reus-code <sergi@reusconstruccions.com>
2022-06-13 19:29:15 +0200

    dfds

--- backendapp/src/app.js
+++ backendapp/src/app.js
@@ -1,8 +1,26 @@
 import express from 'express';
 import taskRoutes from './routes/tasks.js';
+import cors from 'cors';
 
 const app = express();
 
-app.use(taskRoutes)
+const config = {
+    application: {
+        cors: {
+            server: [
+                {
+                    origin: "192.168.18.5:3001", //servidor que deseas que consuma o (*) en caso que sea acceso libre
+                    credentials: true
+                }
+            ]
+        }
+}
+}
 
+
+
+app.use(taskRoutes)
+app.use(cors(
+    config.application.cors.server
+  ));
 export default app
