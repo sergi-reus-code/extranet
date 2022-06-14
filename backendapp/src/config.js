61886813d030a4ef6058076676cfbd8210c9e97c
sergi-reus-code <sergireus28@gmail.com>
2022-06-14 17:46:53 +0200

    sdhjkl

--- backendapp/src/config.js
+++ backendapp/src/config.js
@@ -2,7 +2,7 @@ export const config = {
     host:'db-mysql-ams3-37392-do-user-10958994-0.b.db.ondigitalocean.com',
     user: 'node_user',
     password: 'xfL2FewBsBKY6Tpu',
-    database:'extranet_db'
-    port: 25060,
+    database:'extranet_db',
+    port: 25060
 
 }
