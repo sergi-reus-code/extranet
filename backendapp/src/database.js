78b5aa72495a4ffe8b20c6aa050c6612e529d30e
sergi-reus-code <sergireus28@gmail.com>
2022-06-15 15:45:50 +0200

    ghfdsa

--- backendapp/src/database.js
+++ backendapp/src/database.js
@@ -2,10 +2,7 @@ import mysql from "mysql2/promise.js";
 import { config } from "./config.js";
  
 const connect = async () => {
-  const conn = await mysql.createConnection(config);
-
-const result = conn.query('SELECT 1 + 1'); 
-console.log(result);
+  return await mysql.createConnection(config);
 };
 
 connect();
