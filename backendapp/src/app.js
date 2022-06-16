afd761d3a2a9af260f53ee1143e2e0ee53522335
sergi-reus-code <sergireus28@gmail.com>
2022-06-16 17:03:13 +0200

    dtyfguihojpk

--- backendapp/src/app.js
+++ backendapp/src/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import taskRoutes from './routes/tasks.js';
+import cors from 'cors';
 
+
+import taskRoutes from './routes/tasks.js';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use(taskRoutes);
