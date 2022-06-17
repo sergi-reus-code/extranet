8288fc8fe6db522c7762a6f835130257ced4c669
sergi-reus-code <sergireus28@gmail.com>
2022-06-17 15:31:12 +0200

    trherwghj

--- backendapp/src/routes/tasks.js
+++ backendapp/src/routes/tasks.js
@@ -10,20 +10,57 @@ import {
 
 const router = Router();
 
+/**
+ * @swagger
+ * /tasks:
+ * get:
+ *  summary. Get all tasks
+ */
+
 router.get("/tasks", getTasks)
+
+/**
+ * @swagger
+ * /tasks/count:
+ * get:
+ *  summary. Get total tasks counter 
+ */
 
 router.get("/tasks/count", getTaskCount);
 
+/**
+ * @swagger
+ * /tasks:
+ * get:
+ *  summary. Get a task by id  
+ */
+
 router.get("/tasks/:id", getTask)
+
+/**
+ * @swagger
+ * /tasks:
+ * post:
+ *  summary. save a new task 
+ */
 
 router.post("/tasks/", saveTask)
 
+/**
+ * @swagger
+ * /tasks:
+ * delete:
+ *  summary. delete a task by id
+ */
+
 router.delete("/tasks/:id", deleteTask)
 
-/*router.delete('/tasks/:id', ()=> {
-    console.log("pepe");
-})*/
-
+/**
+ * @swagger
+ * /tasks:
+ * put:
+ *  summary. update a task by id
+ */
 
 router.put("/tasks/:id", updateTask)
 
